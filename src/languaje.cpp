934411038b43d81bd943aa58a8a25febd1c801e4
MiguelAmezquitaM <amezquitamiguel7@gmail.com>
2023-02-25 09:38:30 -0500

    creating enums

--- src/languaje.cpp
+++ src/languaje.cpp
@@ -1,6 +1,42 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
+
+enum class Identifier
+{
+    id
+};
+
+enum class Keyword
+{
+    as,
+    end,
+    begin,
+    state,
+    define,
+    header,
+    section,
+    alphabet,
+    automaton,
+};
+
+enum class Separator
+{
+    endl,
+    comma,
+    open_parethesis,
+    close_parethesis,
+};
+
+enum class Literal
+{
+    string,
+    numeric,
+    boolean,
+    integral,
+    identifier
+};
 
 class LanguageNode
 {
@@ -8,9 +44,20 @@ public:
     LanguageNode(const std::string &token)
         : token(token) {}
 
-    LanguageNode &expect(std::string const &newChild)
+    LanguageNode &expect(Keyword keyword)
     {
-        return children.emplace_back(newChild);
+    }
+
+    LanguageNode &expect(Literal l)
+    {
+    }
+
+    LanguageNode &expect(Separator keyword)
+    {
+    }
+
+    LanguageNode &expect(Identifier l)
+    {
     }
 
     LanguageNode *getChild(const std::string &tokenChild) noexcept
@@ -46,7 +93,7 @@ std::vector<std::string> tokenize(std::string const &chain)
             continue;
         }
 
-        if ("'()\n"s.find(c) != std::string::npos)
+        if ("()\n"s.find(c) != std::string::npos)
         {
             if (!current.empty())
             {
@@ -65,19 +112,44 @@ std::vector<std::string> tokenize(std::string const &chain)
     return tokens;
 }
 
+class Token
+{
+    std::string_view value;
+    std::size_t line;
+};
+
+class Language
+{
+private:
+    inline static std::unordered_map<Keyword, std::string_view> ReservedKeywords{
+        {Keyword::as            , "as"          },
+        {Keyword::end           , "end"         },
+        {Keyword::begin         , "begin"       },
+        {Keyword::state         , "state"       },
+        {Keyword::begin         , "begin"       },
+        {Keyword::header        , "header"      },
+        {Keyword::define        , "define"      },
+        {Keyword::section       , "section"     },
+        {Keyword::alphabet      , "alphabet"    },
+        {Keyword::automaton     , "automaton"   },
+    };
+
+    std::vector<Token> tokens;
+};
+
 int main()
 {
     std::string test = "begin section numeric\n";
 
     LanguageNode root("");
 
-    auto& begin = root.expect("begin");
+    auto &insection = root.expect(Keyword::begin).expect(Keyword::section).expect(Identifier::id).expect(Separator::endl);
 
-    auto &section = begin.expect("section");
+    auto &define = insection.expect(Keyword::define);
+    auto &end = insection.expect(Keyword::end);
 
-    auto &expr = section.expect("\\*\\");
-
-    expr.expect("\n");
+    define.expect(Keyword::alphabet).expect(Literal::string).expect(Keyword::as).expect(Literal::string).expect(Literal::identifier).expect(Separator::endl);
+    define.expect(Keyword::automaton);
 
     LanguageNode *current = &root;
 
